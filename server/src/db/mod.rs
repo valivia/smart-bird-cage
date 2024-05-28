74f218185ffd3879ef7ca55e3932febdeb92d379
valivia <26413018+valivia@users.noreply.github.com>
2024-05-28 11:14:35 +0000

    🐛 Fixed CORS issue

--- server/src/db/mod.rs
+++ server/src/db/mod.rs
@@ -1,16 +1,4 @@
-use rocket::fairing::AdHoc;
 use sqlx::{postgres::PgPoolOptions, Error, Pool, Postgres};
-
-pub fn initialize() -> AdHoc {
-    AdHoc::on_ignite("Connecting to db", |rocket| async {
-        match Database::new().await {
-            Ok(database) => rocket.manage(database),
-            Err(error) => {
-                panic!("Cannot connect to instance:: {:?}", error)
-            }
-        }
-    })
-}
 
 pub struct Database {
     pub pool: Pool<Postgres>,
@@ -20,7 +8,10 @@ pub struct Database {
 impl Database {
     pub async fn new() -> Result<Self, Error> {
         let host = std::env::var("DATABASE_URL").unwrap();
-        let pool = PgPoolOptions::new().max_connections(5).connect(&host).await?;
+        let pool = PgPoolOptions::new()
+            .max_connections(5)
+            .connect(&host)
+            .await?;
         Ok(Self { pool })
     }
 }
