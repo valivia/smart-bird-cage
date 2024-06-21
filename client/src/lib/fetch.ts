0a8fa721d3b755814c10a8f038eb173449ca8d1c
Menno Veerkamp <veerkampmenno@gmail.com>
2024-06-21 19:58:00 +0200

    fix: env

--- client/src/lib/fetch.ts
+++ client/src/lib/fetch.ts
@@ -1,8 +1,10 @@
+import { env } from "$lib/env";
+
 export async function fetchData(url: string) {
     const response = await fetch(url, {
         method: "GET",
         headers: {
-            Authorization: import.meta.env.VITE_AUTHORIZATION,
+            Authorization: env.AUTHORIZATION,
         },
     });
     return response.json();
