dccdb04c49d0602153cfa9e4637de4feee7b16f8
Menno Veerkamp <veerkampmenno@gmail.com>
2024-06-27 15:40:20 +0200

    feat: add descriptions + namechange

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -29,7 +29,8 @@
     let movementValues: number[];
     let soundValues: number[];
 
-    let humidityDescription: string = 'Report helps navigate cumulative growth of community activities. Ideally, the chart should have a growing trend, as stagnating chart signifies a significant decrease of community activity.'
+    let movementDescription: string = 'Intensity of movement where 0 is no movement and 100 is constant movement'
+    let weightDescription: string = 'Weight in grams, only measured when bird is on scale'
 
     const mapData = () => {
         timeStamps = data.map(({ time }) => new Date(time).toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit', hour12: false}));
@@ -47,7 +48,7 @@
         fetchPeriods();
     })
     
-    let timePeriod: string = 'Last month'
+    let timePeriod: string = 'Last 24H'
     let dropdownOpen = false;
     const timePeriodOptions: object = {'Last 24H': 'day', 'Last week': 'week', 'Last month': 'month', 'Last year': 'year'}
     const timePeriodOptionsValues = Object.entries(timePeriodOptions).map(([k]) => `${k}`);
@@ -62,7 +63,7 @@
 
 <main class="m-5 lg:m-20">
     <div class="flex items-center justify-between my-5">
-        <h1 class="text-5xl text-white">Bird Health</h1>
+        <h1 class="text-5xl text-white">Smart birdcage</h1>
         <Button class="inline-flex items-center py-0 text-sm font-medium text-center text-gray-500 bg-transparent dark:text-gray-400 hover:text-gray-900 dark:hover:text-white hover:bg-transparent dark:bg-transparent dark:hover:bg-transparent focus:ring-transparent dark:focus:ring-transparent">{timePeriod}<ChevronDownOutline class="w-2.5 m-2.5 ms-1.5" /></Button>
         <Dropdown classContainer='dark:bg-[#374151] dark:text-white overflow-hidden' class="w-40" offset={-6} bind:open={dropdownOpen}>
             {#each timePeriodOptionsValues as option, i}
@@ -72,11 +73,11 @@
     </div>
     <div class="flex justify-center w-full">
         <div class="grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 max-w-[2560px] w-full">
-            <CustomChart name='Humidity' description={humidityDescription} unit='%' data={humidityValues} {timeStamps} {timePeriod} min={0} max={100} color='#1A56DB' />
+            <CustomChart name='Humidity' unit='%' data={humidityValues} {timeStamps} {timePeriod} min={0} max={100} color='#1A56DB' />
             <CustomChart name='Temperature' unit='°C' data={temperatureValues} {timeStamps} {timePeriod}  color='#f05252' />
             <CustomChart name='Light' unit='LUX' data={lightValues} {timeStamps} {timePeriod} color='#6af7bb'  />
-            <CustomChart name='Weight' unit='g' data={weightValues} {timeStamps} {timePeriod} color='#be8fff'  />
-            <CustomChart name='Movement' unit='amount' data={movementValues} {timeStamps} {timePeriod} color='#fdba3c' />
+            <CustomChart name='Weight' description={weightDescription} unit='g' data={weightValues} {timeStamps} {timePeriod} color='#be8fff'  />
+            <CustomChart name='Movement' description={movementDescription} data={movementValues} {timeStamps} {timePeriod} color='#fdba3c' />
             <CustomChart name='Sound' unit='amount' data={soundValues} {timeStamps} {timePeriod} color='#5deffe' />
         </div>
     </div>
